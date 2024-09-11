7373323314fca0666d5b80701a2d74fea2afec07
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-09-11 22:03:54 +0500

    changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <form action="#!">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+          <button>LogIn</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
