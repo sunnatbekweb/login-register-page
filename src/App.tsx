f6c954296a453df7421a2d8722707e898c591861
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-09-15 22:26:20 +0500

    added registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
 
-interface FormData {
+interface LoginFormData {
   email: string;
   password: string;
 }
 
+interface RegisterFormData {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 function App() {
-  const [loginData, setLoginData] = useState<FormData>({
+  const [loginData, setLoginData] = useState<LoginFormData>({
     email: "",
     password: "",
   });
+
+  const [registerData, setRegisterData] = useState<RegisterFormData>({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [page, setPage] = useState<string | undefined>("login");
 
   const handleLoginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
-      [name]:value,
+      [name]: value,
+    });
+  };
+
+  const handleRegisterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegisterData({
+      ...registerData,
+      [name]: value,
     });
   };
 
@@ -24,9 +48,30 @@ function App() {
     console.log(loginData);
   };
 
+  const handlePageChange = (e: React.MouseEvent<HTMLDivElement>) => {
+    const dataPage = e.currentTarget.dataset.page;
+    setPage(dataPage);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+      <h2>{page === "login" ? "Login" : "Register"}</h2>
+      {page === "register" && (
+        <div>
+          <label>
+            Full Name:
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Enter your full name"
+              value={registerData.fullName}
+              onChange={handleRegisterChange}
+              required
+            />
+          </label>
+          <br />
+        </div>
+      )}
       <label>
         Email:
         <input
@@ -51,6 +96,22 @@ function App() {
         />
       </label>
       <br />
+      {page === "register" && (
+        <div>
+          <label>
+            Confirm Password:
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              value={registerData.confirmPassword}
+              onChange={handleRegisterChange}
+              required
+            />
+          </label>
+          <br />
+        </div>
+      )}
       <div className="remember_forget">
         <label>
           <input type="checkbox" name="remember" id="remember" />
@@ -59,9 +120,15 @@ function App() {
         <a href="#!">Forget password?</a>
       </div>
       <button type="submit">Submit</button>
-      <p  className="noAccount">
-        Dont have an account? <a href="#!">Register</a>
-      </p>
+      {page === "login" ? (
+        <p className="noAccount">
+          Dont have an account? <div data-page="register" onClick={handlePageChange}>Register</div>
+        </p>
+      ) : (
+        <p className="noAccount">
+          Allready have an account? <div data-page="login" onClick={handlePageChange}>Login</div>
+        </p>
+      )}
     </form>
   );
 }
